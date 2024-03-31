af16b91f93d18f126b8fbfb26729a342f35fb338
= <aman@conscent.ai>
2024-03-31 17:08:48 +0530

    fixed deployment breaking file

--- src/Auth/firebaseAuth.ts
+++ src/Auth/firebaseAuth.ts
@@ -1,5 +1,5 @@
 import { initializeApp} from 'firebase/app';
-import { getAuth,sendPasswordResetEmail,confirmPasswordReset } from 'firebase/auth';
+import { getAuth,confirmPasswordReset } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 export const firebaseConfig=
 {
